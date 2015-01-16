94cceb8ec495083d8d776005efce38d733b5d44b
GSISP <pushpalatha.gadiraju@gmail.com>
2015-01-16 23:23:06 +0530

    dsv

--- src/Hello.java
+++ src/Hello.java
@@ -7,34 +7,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		byte num =  (byte) 129; // -128 to 127
 		
-		boolean switch_status=true;
-		char vowel_value='e';
-		
-		String name1="Yogeshwari",name2="Swaraj",name3="jose",name4="Chinmay";
-		
-	//num=num+1; //num-1
-		System.out.println("............ Hello world " + num );
-		System.out.println("............ Hello world " + switch_status );
-		
-		System.out.println("Attendees List for Today Demo : ");
-		System.out.println(name1);
-		System.out.println(name2);
-		System.out.println(name3);
-		System.out.println(name4);
-		
-		
-		double fl=(int)34.568; //int
-		System.out.println((int)34.568);
-		System.out.println(fl);
-		
-		
-		long l=20;
-		System.out.println(l);
-		
-		double d=l;//20.0
-		System.out.println(d);
+		System.out.println("Hello World");
 	}
 
 }
